d8432fc64d01897d1179e63caa45b39c497b6bb1
KyleBPerez <star_wars51389@yahoo.com>
2022-02-15 12:24:49 -0800

    built getAllRoutines function
    added a semi colon to  line 61 in activites.js

--- db/routines.js
+++ db/routines.js
@@ -31,7 +31,39 @@ const getRoutinesWithoutActivities = async () => {
   }
 }
 
+const getAllRoutines = async () => {
+  try {
+    // selects the specified columns from the routines table
+    // and joins the users table to the routines table
+    // where rountines."creatorId" equals user.id
+    const { rows: routines } = await client.query(`
+      SELECT r.id, r."creatorId", r."isPublic", r.name, r.goal, u.username AS "creatorName"
+      FROM routines r
+      JOIN users u ON r."creatorId" = u.id;
+    `)
+    // selects all columns from the activities table
+    // and joins the routines_activites table to the activities table
+    // where routine_activites."activityId" equals activites.id
+    const { rows: activities } = await client.query(`
+      SELECT * 
+      FROM activities a
+      JOIN routine_activities ra ON ra."activityId" = a.id;
+    `)
+
+    routines.forEach((routine) => {
+      routine.activities = activities.filter(
+        (activity) => activity.routineId === routine.id
+      )
+    })
+
+    return routines
+  } catch (err) {
+    throw err
+  }
+}
+
 module.exports = {
   createRoutine,
   getRoutinesWithoutActivities,
+  getAllRoutines,
 }
