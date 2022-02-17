387ad3c7f51b30c3f05dd6c16a1b531baadbc0f9
AaaronPM <aaron.perez989@gmail.com>
2022-02-16 21:15:31 -0800

    api-index express routing get/health completed

--- api/index.js
+++ api/index.js
@@ -1,3 +1,27 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
+
+const express = require('express');
+
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env
+const apiRouter = express.Router();
+const {
+    getAllActivities, 
+} = require('../db')
+
+
+
+apiRouter.get('/health',async(req,res,next)=>{
+    try {
+        res.send({message:'all is well'})
+    } catch (error) {
+        throw error
+    }
+    
+});
+
+apiRouter.use('/users',require('./users'))
+
+module.exports = apiRouter
