62dd62a37206fbc8cb4a24aaaca8d31879d11a15
AaaronPM <aaron.perez989@gmail.com>
2022-02-25 15:40:13 -0800

    finished patch_activities

--- api/activities.js
+++ api/activities.js
@@ -3,7 +3,7 @@ const express = require('express');
 const activitiesRouter = express.Router();
 const {requireUser} = require('./utils')
 const {
-    getAllActivities, createActivity
+    getAllActivities, createActivity, getActivityById, updateActivity
 } = require('../db')
 
 activitiesRouter.get('/',async(req,res,next)=>{
@@ -33,4 +33,23 @@ activitiesRouter.post('/',requireUser, async(req,res,next)=>{
     }
 })
 
+activitiesRouter.patch('/:activityId', requireUser,async(req,res,next)=>{
+    try {
+        const {activityId : id } = req.params
+        const {name , description} = req.body
+        const updateFields = {id,name,description}
+        const activity = await getActivityById(id)
+        if(!activity){
+            next({
+                name:'ActivityError',
+                message:'This activity does not exist'
+            })
+            return
+        }
+        const updatedActivity = await updateActivity(updateFields)
+        res.send(updatedActivity)
+    } catch (error) {
+        next(error)
+    }
+})
 module.exports = activitiesRouter
