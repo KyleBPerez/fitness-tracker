61373fbec75593e151f70f8bd297dc96c110da30
KyleBPerez <star_wars51389@yahoo.com>
2022-02-26 15:16:51 -0800

    tried to smooth out the error handling inside both api/activities.js and db/activities.js

--- api/activities.js
+++ api/activities.js
@@ -1,77 +1,61 @@
-const express = require("express");
+const express = require('express')
 
-const activitiesRouter = express.Router();
-const { requireUser } = require("./utils");
+const activitiesRouter = express.Router()
+const { requireUser } = require('./utils')
 const {
   getAllActivities,
   createActivity,
   getActivityById,
   updateActivity,
   getPublicRoutinesByActivity,
-} = require("../db");
+} = require('../db')
 
-activitiesRouter.get("/", async (req, res, next) => {
+activitiesRouter.get('/', async (req, res, next) => {
   try {
-    const activities = await getAllActivities();
-    res.send(activities);
+    const activities = await getAllActivities()
+    res.send(activities)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-activitiesRouter.post("/", requireUser, async (req, res, next) => {
+activitiesRouter.post('/', requireUser, async (req, res, next) => {
   try {
-    const activity = req.body;
-    if (!activity.name || !activity.description) {
+    const { name, description } = req.body
+    if (!name || !description) {
       next({
-        name: "incompletebodyerror",
-        message: "Requires a username and description",
-      });
-      return;
+        name: 'incompletebodyerror',
+        message: 'Requires a username and description',
+      })
+      return
     }
-    const newActivity = await createActivity(activity);
-    res.send(newActivity);
+    const newActivity = await createActivity({ name, description })
+    res.send(newActivity)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-activitiesRouter.patch("/:activityId", requireUser, async (req, res, next) => {
+activitiesRouter.patch('/:activityId', requireUser, async (req, res, next) => {
   try {
-    const { activityId: id } = req.params;
-    const { name, description } = req.body;
-    const updateFields = { id, name, description };
-    const activity = await getActivityById(id);
-    if (!activity) {
-      next({
-        name: "ActivityError",
-        message: "This activity does not exist",
-      });
-      return;
-    }
-    const updatedActivity = await updateActivity(updateFields);
-    res.send(updatedActivity);
+    const { activityId: id } = req.params
+    const { name, description } = req.body
+    const updateFields = { id, name, description }
+    const updatedActivity = await updateActivity(updateFields)
+    res.send(updatedActivity)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-activitiesRouter.get("/:activityId/routines", async (req, res, next) => {
-  const { activityId: id } = req.params;
+activitiesRouter.get('/:activityId/routines', async (req, res, next) => {
+  const { activityId: id } = req.params
   try {
-    const activity = await getActivityById(id);
-    if (!activity) {
-      next({
-        name: "ActivityError",
-        message: "This activity does not exist",
-      });
-
-      return;
-    }
-    const routineActivities = await getPublicRoutinesByActivity(activity);
-    res.send(routineActivities);
+    const activity = await getActivityById(id)
+    const routineActivities = await getPublicRoutinesByActivity(activity)
+    res.send(routineActivities)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-module.exports = activitiesRouter;
+})
+module.exports = activitiesRouter
