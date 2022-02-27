dc9b7e51357c4f194adab331f296dfe2e8785b9e
KyleBPerez <star_wars51389@yahoo.com>
2022-02-26 16:00:49 -0800

    uncommented user check inside getUserByUsername

--- db/users.js
+++ db/users.js
@@ -56,11 +56,11 @@ const getUserByUsername = async (username) => {
       [username]
     )
 
-    // if (!user)
-    //   throw {
-    //     name: `FindUserByUsernameError`,
-    //     message: `Unable to find a user with the username: ${username}`,
-    //   }
+    if (!user)
+      throw {
+        name: `FindUserByUsernameError`,
+        message: `Unable to find a user with the username: ${username}`,
+      }
 
     return user
   } catch (err) {
